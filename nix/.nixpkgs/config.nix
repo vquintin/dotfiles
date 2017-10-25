899fc2c94e3f8d46be99053032ab196e9d9ee5f6
Virgile Quintin <virgile.quintin@gmail.com>
2017-10-25 23:42:56 +0200

    Merge with laptop nixpkgs

--- nix/.nixpkgs/config.nix
+++ nix/.nixpkgs/config.nix
@@ -1,12 +1,47 @@
 {
+  allowUnfree = true;
+
+  hardware.opengl.driSupport32Bit = true;
+  permittedInsecurePackages = [
+    "webkitgtk-2.4.11"
+  ];
+
   packageOverrides = pkgs_: with pkgs_; {
     my_vim = import ./vim-config { inherit pkgs ; };
+
+    home_laptop = with pkgs; buildEnv {
+      name = "home-laptop";
+      paths = [
+        basic_home
+        blueman
+        cbatticon
+        networkmanagerapplet
+        rfkill
+        xorg.xbacklight
+      ];
+    };
+
+    basic_home = with pkgs; buildEnv {
+      name = "basic-home";
+      paths = [
+        basic_gui_env
+        dev_env
+        games
+        internet_prgms
+        media_picture
+        media_sound
+        media_video
+      ];
+    };
 
     dev_env = with pkgs; buildEnv {
       name = "dev-env";
       paths = [
         my_vim
         git
+        go
+        python
+        stack
       ];
     };
 
@@ -34,10 +69,14 @@
         firefox
         gnome3.file-roller
         gui_utils
+        lightdm
+        pasystray
         pavucontrol
+        powerline-fonts
         termite
         thunderbird
         xfce.thunar
+        xfce.thunar_volman
         xscreensaver
       ];
     };
@@ -45,7 +84,7 @@
     basic_env = with pkgs; buildEnv {
       name = "basic-env";
       paths = [
-	cli_utils
+        cli_utils
         zsh
       ];
     };
@@ -54,8 +93,11 @@
       name = "cli-utils";
       paths = [
         file
+        fzf
         htop
         lsof
+        pciutils
+        pv
         stow
         tree
       ];
@@ -65,6 +107,16 @@
       name = "gui-utils";
       paths = [
         baobab
+        gparted
+        glxinfo
+      ];
+    };
+
+    internet_prgms = with pkgs; buildEnv {
+      name = "internet-pgrms";
+      paths = [
+        qbittorrent
+        remmina
       ];
     };
 
@@ -72,6 +124,53 @@
       name = "media-text";
       paths = [
         calibre
+        libreoffice
+        open-dyslexic
+        pandoc
+        texlive.combined.scheme-full
+      ];
+    };
+
+    media_picture = with pkgs; buildEnv {
+      name = "media-picture";
+      paths = [
+        feh
+        gphoto2
+        gimp
+        rawtherapee
+        shotwell
+        ufraw
+        xfce.ristretto
+      ];
+    };
+
+    media_sound = with pkgs; buildEnv {
+      name = "media-sound";
+      paths = [
+        clementine
+      ];
+    };
+
+    media_video = with pkgs; buildEnv {
+      name = "media-video";
+      paths = [
+        smplayer
+        vdpauinfo
+        vlc
+      ];
+    };
+
+    games = with pkgs; buildEnv {
+      name = "games";
+      paths = [
+        steam
+      ];
+    };
+
+    accounting = with pkgs; buildEnv {
+      name = "accounting";
+      paths = [
+        gnucash26
       ];
     };
   };
