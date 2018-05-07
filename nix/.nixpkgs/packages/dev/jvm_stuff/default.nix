fcb552a053609b824588b2f8cd8f4c498479e188
Virgile Quintin <vquintin@mediarithmics.com>
2018-05-07 17:03:09 +0200

    Rollback to intellij 2017.3

--- nix/.nixpkgs/packages/dev/jvm_stuff/default.nix
+++ nix/.nixpkgs/packages/dev/jvm_stuff/default.nix
@@ -1,10 +1,18 @@
 {pkgs}:
-
+let
+  oldnixpkgs = builtins.fromJSON (builtins.readFile ./oldnixpkgs.json);
+  src = pkgs.fetchFromGitHub {
+    owner = "NixOS";
+    repo = "nixpkgs";
+    inherit (oldnixpkgs) rev sha256;
+  };
+  oldPkgs = import src { };
+in
 with pkgs; buildEnv {
   name = "jvm-stuff";
   paths = [
     ammonite
-    jetbrains.idea-community
+    oldPkgs.jetbrains.idea-community
     openjdk
     scala
     sbt
